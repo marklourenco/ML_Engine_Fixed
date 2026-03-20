271144341ca2668f5567da771d4e7591f9b4bd25
Mark Lourenco <markjlourencor@gmail.com>
2026-03-20 01:35:32 -0700

    Renamed Final Assignment to 24_HelloAnimationFinal

--- VGP334/24_HelloAnimationFinal/WinMain.cpp
+++ VGP334/24_HelloAnimationFinal/WinMain.cpp
@@ -5,7 +5,7 @@ using namespace ML_Engine;
 int WINAPI WinMain(HINSTANCE instance, HINSTANCE, LPSTR, int)
 {
 	AppConfig config;
-	config.appName = L"Hello Predator Final";
+	config.appName = L"Hello Animation Final";
 	
 	App& myApp = MainApp();
 	myApp.AddState<GameState>("GameState");
